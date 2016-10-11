c2e069598437c93e7dcea9ab5a5e6a0f32243588
Sal Mangano <smangano@into-technology.com>
2016-10-11 11:20:21 +0000

    Enhanced registry interface

--- include/theoria/core/Registry.h
+++ include/theoria/core/Registry.h
@@ -16,6 +16,7 @@ private:
 
     using FactoryMap = std::map<CompFactoryKey, ComponentFactory> ;
     using ComponentMap = util::densemap<CompId, Component*> ;
+    using ComponentXrefMap = std::multimap<CompFactoryKey, CompId> ;
 
     Registry() {}
     Registry(const Registry&) ;
@@ -45,16 +46,40 @@ public:
      * 2) If there are multiple factories and none has been used then use the default factory whose subtype==type
      * 3) If there are multiple factories and none has been used and there is no default, use the first one
      * 4) If there are multiple factories use the first one that has already been used
+     *
+     * @type the type name to use for look up
+     *
+     * @execept std::runtime_error if type not found
      */
     Component* createComponent(const TypeName& type) ;
+
+    /*
+     * Create a component of the specified type and subtype. 
+     *
+     * @type the type name to use for look up (primary key)
+     * @subtype the sub type name to use for look up (secondary key)
+     *
+     * @throw std::runtime_error if factory for both type and subtype does not exist
+     */
     Component* createComponent(const TypeName& type, const SubTypeName& subtype) ;
+
+    FactoryMap::iterator beginFact() ;
+    FactoryMap::iterator endFact() ;
+    FactoryMap::iterator findFact(const TypeName& type) ;
+    FactoryMap::iterator findfact(FactoryMap::iterator start, bool (*predicate)(FactoryMap::value_type& v))  ;
+
+    ComponentMap::iterator beginComp() ;
+    ComponentMap::iterator endComp() ;
+
+    ComponentMap::const_iterator beginComp() const ;
+    ComponentMap::const_iterator endComp() const ;
 
 private:
 
     CompId _nextId ;
     FactoryMap _factories ; 
     ComponentMap _components ;
-
+    ComponentXrefMap _xref ;
 } ;
 
 
